8cc6532d449e6c481172702cf167e0e2fb66960c
Brandon Johnson <157184929+Bozer172@users.noreply.github.com>
2024-09-09 19:44:20 -0400

    Update write_ids.c

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "Bozer172";
+    char STUDENT_ID[] = "985889249";
 
     return 0 ;
 }
